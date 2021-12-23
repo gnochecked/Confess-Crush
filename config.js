2e8431a7ac78f547ceedcace415ec1a4e2e709e2
gnochecked <96575067+gnochecked@users.noreply.github.com>
2021-12-23 18:05:16 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,11 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    titleWeb: "NVQN",
+    introTitle: 'Nguyễn Vũ Quỳnh Như',
+    introDesc: `Anh biết là em yêu anh nhiều lắm ahihi`,
+    btnIntro: '^^nhấn vào đây đi bé yêu^^',
+    title: 'em mê anh trước đúng không 🥰',
+    desc: 'moa moa ',
+    btnYes: 'Phải <33',
     btnNo: 'Không nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
